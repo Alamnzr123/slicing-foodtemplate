59cc29f242aedcfdf20a86be542913961b483d6f
Rahmad Alamsyah Nazaruddin <54619997+Alamnzr123@users.noreply.github.com>
2022-05-14 22:48:02 +0700

    Edit Login

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,5 +1,5 @@
 import "../App.css";
-import react from "react";
+import React from "react";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
